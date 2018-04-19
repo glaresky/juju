ee4136f1bff611abb818b1ca9871597202ba5a58
Ju <glaresky@gmail.com>
2018-04-19 22:58:25 +0900

    fix #3

--- js/grayscale.js
+++ js/grayscale.js
@@ -32,11 +32,11 @@
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
+    // if ($("#mainNav").offset().top > 100) {
+    //   $("#mainNav").addClass("navbar-shrink");
+    // } else {
+    //   $("#mainNav").removeClass("navbar-shrink");
+    // }
   };
   // Collapse now if page is not at top
   navbarCollapse();
